59fb0e94d5cb62d116af0614ba8570765aaa04fb
AkmalDoghar <160627372+AkmalDoghar@users.noreply.github.com>
2026-08-28 09:33:25 +0500

    create chatify app

--- server/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { uploadFile } = require('../controllers/uploadController');
-const { protect } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.post('/', protect, uploadFile);
-
-module.exports = router;
